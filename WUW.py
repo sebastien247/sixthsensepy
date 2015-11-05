33f4460e94d6ab9122782e4c97c6c0ca1b2c22a0
Sebastien Duboc <seb.duboc@gmail.com>
2015-11-05 10:35:47 +0100

    Added UI

--- WUW.py
+++ WUW.py
@@ -1,22 +1,189 @@
-#!/usr/bin/python
-from gi.repository import Gtk as gtk
+#coding=utf-8
 
-class WUW:
+import wx
+import threading
+import thread
+import time
+import os
+import math
+from classes.Value import Value
+from classes.PointR import PointR
+from classes.GeometricRecognizer import GeometricRecognizer
+from classes.NBestList import NBestList
 
-    def __init__(self):
-        interface = gtk.Builder()
-        interface.add_from_file('wuw.glade')
 
-        self.myLabel = interface.get_object("WUW - SixthSense By 3IL")
-        interface.connect_signals(self)
+class WuwPanel(wx.Panel):
+    Width = Value.WuwWidth
+    Height = Value.WuwHeight
+    def __init__(self, parent):
+        print "WuwPanel.__init__"
+        self.Grid = self.Width / 100
+        wx.Panel.__init__(self, parent)
 
-        print interface
+        ###
+        #TabPage
+        self.btnShowHide=wx.Button(self,pos=(self.Width-self.Grid*5,self.Grid),
+                                 size=(3*self.Grid,3*self.Grid))
+        self.btnExit=wx.Button(self,label="Exit",pos=(self.Width-self.Grid*9,self.Grid),
+                             size=(3*self.Grid,3*self.Grid))
+        self.tabSettings=wx.Notebook(self,pos=(self.Width-self.Grid*62,self.Grid*3),
+                                   size=(60*self.Grid,20*self.Grid))
+        self.pictureBoxDisplay=wx.StaticBox(self,pos=(self.Width-self.Grid*66,self.Grid*23),
+                                             size=(64*self.Grid,48*self.Grid))
+        #self.tabSettings.SetTabSize((self.Grid,1.5*self.Grid))
+        self.tabPageCamera=wx.Panel(self.tabSettings)
+        self.tabPageTokens=wx.Panel(self.tabSettings)
+        self.tabPageApps=wx.Panel(self.tabSettings)
+        self.tabSettings.AddPage(self.tabPageCamera, "Camera")
+        self.tabSettings.AddPage(self.tabPageTokens, "Tokens")
+        self.tabSettings.AddPage(self.tabPageApps, "Apps")
+        ##TabPageCamera
+        self.comboBoxCameras=wx.ComboBox(self.tabPageCamera,value="Select A Camera",
+                                       pos=(0,0), size=(25*self.Grid,2*self.Grid))
+        self.lblCameraInfo=wx.StaticText(self.tabPageCamera,label="No Camera Selected",
+                                       pos=(0,3*self.Grid),size=(25*self.Grid,2*self.Grid))
+        self.buttonCameraProperties=wx.Button(self.tabPageCamera,label="Adjust Camera Properties",
+                                            pos=(0,5*self.Grid),size=(23*self.Grid,2*self.Grid))
+        self.labelCameraFPS=wx.StaticText(self.tabPageCamera,label="Current FPS:",
+                                        pos=(0,7*self.Grid),size=(8*self.Grid,2*self.Grid))
+        self.labelCameraFPSValue=wx.StaticText(self.tabPageCamera,label="0.00",
+                                             pos=(8*self.Grid,7*self.Grid),
+                                             size=(6*self.Grid,2*self.Grid))
+        self.checkBoxCameraFPSLimit=wx.CheckBox(self.tabPageCamera,label="Limit Frames Per Second",
+                                              pos=(0,9*self.Grid),
+                                              size=(16*self.Grid,2*self.Grid))
+        self.CameraFPSLimit=wx.TextCtrl(self.tabPageCamera,value="30",
+                                      pos=(17*self.Grid,9*self.Grid),
+                                      size=(6*self.Grid,2*self.Grid))
+        self.lblRecord=wx.StaticText(self.tabPageCamera,label="[Recording]",
+                                   pos=(41*self.Grid,1*self.Grid),
+                                   size=(8*self.Grid,2*self.Grid))
+        self.btnRecord=wx.Button(self.tabPageCamera,label="RECORD",
+                               pos=(42*self.Grid,3*self.Grid),
+                               size=(6*self.Grid,2*self.Grid))
+        self.btnLoad=wx.Button(self.tabPageCamera,label="LOAD",
+                             pos=(42*self.Grid,5*self.Grid),
+                             size=(6*self.Grid,2*self.Grid))
+        self.btnView=wx.Button(self.tabPageCamera,label="VIEW",
+                             pos=(42*self.Grid,7*self.Grid),
+                             size=(6*self.Grid,2*self.Grid))
+        self.btnClear=wx.Button(self.tabPageCamera,label="CLEAR",
+                              pos=(42*self.Grid,9*self.Grid),
+                              size=(6*self.Grid,2*self.Grid))
+        ##TabPageTokens
+        self.buttonMarkerAdd=wx.Button(self.tabPageTokens,label="New Marker",
+                                     pos=(0,0),size=(10*self.Grid,2*self.Grid))
+        self.comboBoxMarkers=wx.ComboBox(self.tabPageTokens,value="Edit Existing Marker",
+                                       pos=(11*self.Grid,0),
+                                       size=(14*self.Grid,2*self.Grid))
+        self.lblTotalMarker=wx.StaticText(self.tabPageTokens,label="Number of markers:",
+                                        pos=(26*self.Grid,0),
+                                        size=(13*self.Grid,2*self.Grid))
+        self.lblMarkerCount=wx.StaticText(self.tabPageTokens,label="0",
+                                        pos=(40*self.Grid,0),
+                                        size=(4*self.Grid,2*self.Grid))
+        self.buttonMarkerSave=wx.Button(self.tabPageTokens,label="Save M",
+                                      pos=(45*self.Grid,0),
+                                      size=(6*self.Grid,2*self.Grid))
+        self.buttonMarkerLoad=wx.Button(self.tabPageTokens,label="Load M",
+                                      pos=(52*self.Grid,0),
+                                      size=(6*self.Grid,2*self.Grid))
+        self.lblMarkerControl=wx.StaticText(self.tabPageTokens,label="No Marker Selected",
+                                          pos=(0,3*self.Grid),
+                                          size=(58*self.Grid,2*self.Grid))
+        self.buttonMarkerRemove=wx.Button(self.tabPageTokens,label="Remove This Marker",
+                                        pos=(0,5*self.Grid),
+                                        size=(20*self.Grid,2*self.Grid))
+        self.checkBoxMarkerHighlight=wx.CheckBox(self.tabPageTokens,label="Highlight Marker",
+                                               pos=(0,7*self.Grid),
+                                               size=(16*self.Grid,2*self.Grid))
+        self.checkBoxMarkerSmoothing=wx.CheckBox(self.tabPageTokens,label="Smooth Marker Data",
+                                               pos=(0,9*self.Grid),
+                                               size=(16*self.Grid,2*self.Grid))
+        self.labelMarkerThresh=wx.StaticText(self.tabPageTokens,label="Marker Threshold:",
+                                           pos=(0,11*self.Grid),
+                                            size=(15*self.Grid,2*self.Grid))
+        self.MarkerThresh=wx.TextCtrl(self.tabPageTokens,value="0",
+                                    pos=(15*self.Grid,11*self.Grid),
+                                    size=(5*self.Grid,2*self.Grid))
+        self.labelMarkerData=wx.TextCtrl(self.tabPageTokens,pos=(21*self.Grid,5*self.Grid),
+                                       size=(37*self.Grid,8*self.Grid))
+        self.labelMarkerData.SetEditable(False)
+        ##TabPageApps
+        self.labelDemoInstructions=wx.TextCtrl(self.tabPageApps,pos=(44*self.Grid,0),
+                                             size=(16*self.Grid,16*self.Grid))
+        self.labelDemoInstructions.SetEditable(False)
+        self.buttonClockDemo=wx.Button(self.tabPageApps,label="Clock",pos=(1*self.Grid,1*self.Grid),
+                                       size=(8*self.Grid,2*self.Grid))
+        self.buttonPhotoDemo=wx.Button(self.tabPageApps,label="Photo",pos=(1*self.Grid,4*self.Grid),
+                                       size=(8*self.Grid,2*self.Grid))
+        self.buttonWeatherDemo=wx.Button(self.tabPageApps,label="Weather",pos=(1*self.Grid,7*self.Grid),
+                                         size=(8*self.Grid,2*self.Grid))
 
+        #Label
+        self.labelM=wx.StaticText(self, label=" M", pos=(4*self.Grid,self.Grid),
+                                size=(2*self.Grid,2*self.Grid))
+        self.labelN=wx.StaticText(self, label=" N", pos=(4*self.Grid,4*self.Grid),
+                                size=(2*self.Grid,2*self.Grid))
+        self.labelO=wx.StaticText(self, label=" O", pos=(1*self.Grid,self.Grid),
+                                size=(2*self.Grid,2*self.Grid))
+        self.labelP=wx.StaticText(self, label=" P", pos=(1*self.Grid,4*self.Grid),
+                                size=(2*self.Grid,2*self.Grid))
+        self.labelM.SetBackgroundColour("ORANGE RED")
+        self.labelO.SetBackgroundColour("ORANGE RED")
+        self.labelN.SetBackgroundColour("RED")
+        self.labelP.SetBackgroundColour("RED")
+        self.labelDemoName=wx.StaticText(self, label="WUW", pos=(7*self.Grid,self.Grid),
+                                       size=(4*self.Grid,2*self.Grid))
+        self.lblResult=wx.StaticText(self, label="Test", pos=(12*self.Grid,self.Grid),
+                                   size=(12*self.Grid,2*self.Grid))
 
-    def on_mainWindow_destroy(self, widget):
-        gtk.main_quit()
+        #DemoBox
+        self.BoxClock=wx.StaticBox(self,pos=(40*self.Grid,30*self.Grid),
+                                   size=(20*self.Grid,20*self.Grid))
+        self.BoxClock.threadTime=None
+        self.BoxWeather=wx.StaticBox(self,pos=(40*self.Grid,20*self.Grid),
+                                     size=(20*self.Grid,40*self.Grid))
+        self.BoxPhoto=wx.StaticBox(self,pos=(18*self.Grid,16*self.Grid),
+                                   size=(64*self.Grid,48*self.Grid))
 
+        ###Global Variables
+        self.__touchlessMgr = None
+        self.__fAddingMarker = False
+        self.__inBoxArea = False
+        self.m = None
+        self.n = None
+        self.o = None
+        self.p = None
+        self.__isDown = False
+        self.__recording = False
+        self.__rec = GeometricRecognizer()
+        self.__points = []
+        self.__show_settings = False
+        self.__markerCenter = None
+        self.__markerRadius = 0
+        self.__drawSelectionAdornment = False
+        self.__addedMarkerCount = 0
+        self.__latestFrame = None
+        self.__latestFrameTime = time.time()
+        self.__latestFrameTimeSegment = False
+        self.__ratioScreenCameraHeight = 0
+        self.__ratioScreenCameraWidth = 0
+        self.clockDemo = False
+        self.photoDemo = False
+        self.weatherDemo = False
+
+        ###Load
+        self.BoxClock.Hide()
+        self.BoxWeather.Hide()
+        self.BoxPhoto.Hide()
+
+def main():
+    app = wx.App(False)
+    frame = wx.Frame(None, wx.ID_ANY, "WUW", size=(WuwPanel.Width,WuwPanel.Height))
+    panel = WuwPanel(frame)
+    frame.Show(True)
+    app.MainLoop()
 
 if __name__ == "__main__":
-    WUW()
-    gtk.main()
+    main()
