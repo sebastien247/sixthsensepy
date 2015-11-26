f8a8adcecc1cc38fbbaaf39324953ff0fbbac477
Sebastien <sebastien247@gmail.com>
2015-11-26 09:00:48 +0100

    Updated HSV values, green marcker

--- TouchlessLib.py
+++ TouchlessLib.py
@@ -343,8 +343,8 @@ class TouchlessMgr:
 
         # ---------- Green Mask ---------- #
         #- mask
-        self.l_lower_green = np.array([55,125,125])
-        self.l_upper_green = np.array([70,255,255])
+        l_lower_green = np.array([60,125,125])
+        l_upper_green = np.array([90,255,255])
         # -------- End Green Mask -------- #
 
         # ---------- Blue Mask ---------- #
