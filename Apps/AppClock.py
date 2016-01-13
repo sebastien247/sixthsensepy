d8e80c130cef49a9e388725a5d90375b8fa9e61c
Florent <florentm007@gmail.com>
2016-01-13 13:45:15 +0100

    fixed clock with new design

--- Apps/AppClock.py
+++ Apps/AppClock.py
@@ -1,9 +1,12 @@
 from App import App
 import wx
+import time
 
 class AppClock(App):
     def __init__(self, WuwPanel):
         App.__init__(self, WuwPanel)
+
+        self.timeUpdating = []
 
         self.name = "clock"
         self.button = wx.Button(self.tabPageApps,label="Clock",pos=(1*self.Grid,1*self.Grid),
@@ -18,15 +21,21 @@ class AppClock(App):
         
         self.after_init()
 
-    def show_time(self, event):
-        dc = wx.PaintDC(self.box)
-        dc.Clear()
-        dc.DrawText(time.strftime("%H:%M:%S", time.localtime(time.time())),
-                    8*self.Grid,8*self.Grid)
+    def show_time(self):
+        for i in self.timeUpdating:
+            i.Destroy()
+        self.timeUpdating = []
+        font = wx.Font(18, wx.DECORATIVE, wx.NORMAL, wx.NORMAL)
+        self.clock = wx.StaticText(self.box,-1, str(time.strftime("%H:%M:%S", time.localtime(time.time()))),pos=(self.Grid*50/10,self.Grid*80/10))
+        self.clock.SetFont(font)
+        self.clock.SetForegroundColour(wx.Colour(255,255,255))
+        timeValue = self.timeUpdating.append(self.clock)
+        wx.CallLater(1000, self.show_time)
 
     def start(self):
         self.box.threadTime = self.wuw.ThreadTime("time", 1, self.box)
         self.box.threadTime.start()
+        self.show_time()
         self.box.Show()
 
     def end(self):
